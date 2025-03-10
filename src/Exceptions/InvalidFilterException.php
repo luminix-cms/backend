a7a44d06faa72382fd3ebdad446671ebbefb537f
Bruno Polo <brunopolo@poli.ufrj.br>
2025-03-10 10:58:16 -0300

    fix: organize files // add export ignore to test files // fixes deprecated implicit nullable parameter

--- src/Exceptions/InvalidFilterException.php
+++ src/Exceptions/InvalidFilterException.php
@@ -6,7 +6,7 @@ use Exception;
 
 class InvalidFilterException extends Exception
 {
-    public function __construct($message = 'Invalid filter provided.', $code = 0, Exception $previous = null)
+    public function __construct($message = 'Invalid filter provided.', $code = 0, ?Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
