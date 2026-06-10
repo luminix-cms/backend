2adbc4eca7ebc20e61fec688fa43aeb8310b3a1c
Bruno Polo <brunopolo@poli.ufrj.br>
2026-06-10 19:47:37 -0300

    test(feature): relax strict JSON assertion in RestApiTest

--- workbench/app/Tests/Feature/RestApiTest.php
+++ workbench/app/Tests/Feature/RestApiTest.php
@@ -47,7 +47,7 @@ class RestApiTest extends TestCase
         // the Gate, but the API will still be protected
         $this->json('GET', '/luminix-api/to-dos')
             ->assertStatus(200)
-            ->assertContent('{"data":[],"meta":{"current_page":1,"from":null,"last_page":1,"per_page":15,"to":null,"total":0,"links":[{"url":null,"label":"&laquo; Previous","active":false},{"url":"http:\/\/localhost\/luminix-api\/to-dos?page=1","label":"1","active":true},{"url":null,"label":"Next &raquo;","active":false}]},"links":{"first":"http:\/\/localhost\/luminix-api\/to-dos?page=1","last":"http:\/\/localhost\/luminix-api\/to-dos?page=1","prev":null,"next":null}}');
+            ->assertJsonPath("data", []);
 
         $this->json('GET', '/luminix-api/to-dos/1')
             ->assertStatus(401);
